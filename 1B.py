429b571f467475ac598d9f99e32ea06aafef9df9
Manuel_Lee <Manuel_Lee>
2017-11-16 04:13:55 -1200

    manuel

---
+++ 1B.py
@@ -0,0 +1,8 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/HP/Desktop/emmanuel 2.py =================
+please enter value for x:  5
+please enter value for y: 8
+('the result is ', 2.8)
+>>> 
